223d507f4f991fef5c0c1d85f96377b61e98ca0c
Hwanseok Kang (Barth) <hwanseok.kang@gmail.com>
2024-07-18 23:26:54 +0200

    chore: Fix proxy ip problem

--- deploy-key-update.sh
+++
@@ -1,2 +0,0 @@
-ssh-agent sh -c "ssh-add -D; ssh-add ~/.ssh/lims-client; git submodule update LIMS-web-client"
-ssh-agent sh -c "ssh-add -D; ssh-add ~/.ssh/lims-server; git submodule update --remote LIMS-api-server"
